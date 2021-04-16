325875a6d356734b00f152ddaa7e48a42668d124
Gabriel Nascimento <gabriel.nascimento@dcomp.ufs.br>
2021-04-15 21:28:01 -0300

    terminando slide

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,14 @@
-$('.slider-principal').slick();
+$(document).ready(function(){
+    $('.slider-principal').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4500
+      });
+});
+
+
+
